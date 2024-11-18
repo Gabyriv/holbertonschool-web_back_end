1f8b0efa2521faef889ba7aa8e2c5266c45ee0a8
Gabriel <gabyrivm06@gmail.com>
2024-11-18 13:45:36 -0400

    Combine

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    student.grade = newGrades.find((grade) => grade.studentId === student.id);
+    if (!student.grade) {
+      student.grade = { studentId: student.id, grade: 'N/A' };
+    }
+    return student;
+  });
+}
