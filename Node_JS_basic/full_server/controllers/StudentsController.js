30c7b455a274a4bf483188bea2e3c2fecc09689a
Gabriel <gabyrivm06@gmail.com>
2025-01-23 14:33:51 -0400

    Organize a complex HTTP server using Express

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,46 +1,42 @@
-const { readDatabase } = require('../utils');
+import readDatabase from '../utils';
 
 class StudentsController {
-  static async getAllStudents(request, response) {
-    try {
-      const fields = await readDatabase(process.argv[2]);
-      let output = 'This is the list of our students\n';
-      
-      // Sort fields alphabetically (case insensitive)
-      const sortedFields = Object.keys(fields).sort((a, b) => 
-        a.toLowerCase().localeCompare(b.toLowerCase())
-      );
-      
-      sortedFields.forEach(field => {
-        output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+  static getAllStudents(request, response, database) {
+    readDatabase((database))
+      .then((fields) => {
+        const students = [];
+        let message;
+
+        students.push('This is the list of our students');
+
+        for (const key of Object.keys(fields)) {
+          message = `Number of students in ${key}: ${fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(message);
+        }
+        response.send(200, `${students.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
       });
-      
-      response.status(200).send(output.trim());
-    } catch (error) {
-      response.status(500).send(error.message);
-    }
   }
 
-  static async getAllStudentsByMajor(request, response) {
+  static getAllStudentsByMajor(request, response, database) {
     const { major } = request.params;
-    
+
     if (major !== 'CS' && major !== 'SWE') {
-      response.status(500).send('Major parameter must be CS or SWE');
-      return;
-    }
-    
-    try {
-      const fields = await readDatabase(process.argv[2]);
-      if (!fields[major]) {
-        response.status(500).send('Major not found');
-        return;
-      }
-      
-      response.status(200).send(`List: ${fields[major].join(', ')}`);
-    } catch (error) {
-      response.status(500).send(error.message);
+      response.send(500, 'Major parameter must be CS or SWE');
+    } else {
+      readDatabase(database)
+        .then((fields) => {
+          const students = fields[major];
+
+          response.send(200, `List: ${students.join(', ')}`);
+        })
+        .catch(() => response.send(500, 'Cannot load the database'));
     }
   }
 }
 
-module.exports = StudentsController;
+export default StudentsController;
--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,24 +1,34 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 
-const readDatabase = async (filePath) => {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    const lines = data.trim().split('\n');
-    const students = lines.slice(1).filter(line => line.trim()); // Remove header and empty lines
-    
-    const fields = {};
-    students.forEach(student => {
-      const [firstname, lastname, age, field] = student.split(',');
-      if (!fields[field]) {
-        fields[field] = [];
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
-      fields[field].push(firstname);
-    });
-    
-    return fields;
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
-};
+      const content = data.toString().split('\n');
 
-module.exports = { readDatabase };
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
+    });
+  });
+}
+
+export default readDatabase;
