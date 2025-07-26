9a325f987518e012310b362ba795bd5351f0114c
Gabriel <gabyrivm06@gmail.com>
2025-07-26 18:41:33 -0400

    Create logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -17,6 +17,7 @@ def get_logger() -> logging.Logger:
     """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
+    logger.propagate = False
     handler = logging.StreamHandler()
     handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
     logger.addHandler(handler)
