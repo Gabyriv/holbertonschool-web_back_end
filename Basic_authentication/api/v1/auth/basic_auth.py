b28e152cc7cf2e625a85f06a5e442a0aacfc1faf
Gabriel <gabyrivm06@gmail.com>
2025-07-27 15:44:48 -0400

    Basic - User credentials

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -47,3 +47,18 @@ class BasicAuth(Auth):
             return decoded_header.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> tuple[str, str]:
+        """
+        Extract the user credentials
+        from the decoded Base64 authorization header
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(":", 1))
