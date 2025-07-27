0a10498b6acd45e1321259cb44365db771cc87e5
Gabriel <gabyrivm06@gmail.com>
2025-07-27 14:29:37 -0400

    Auth class

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -17,20 +17,17 @@ class Auth:
         """
         pass
 
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Require authentication for a given path
         """
         return False
 
-
     def authorization_header(self, request=None) -> str:
         """
         Authorization header
         """
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
