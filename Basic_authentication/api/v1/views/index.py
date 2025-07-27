69cb34e352712aa5ff47ef4ce57ae5a3477b8371
Gabriel <gabyrivm06@gmail.com>
2025-07-27 14:13:01 -0400

    Error handler: Forbidden

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -34,3 +34,13 @@ def unauthorized() -> str:
         - 401 status code
     """
     return abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+      GET /api/v1/forbidden
+      Return:
+        - 403 status code
+    """
+    return abort(403)
