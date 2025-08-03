cab3cd1ce8ab607f515a342c56817802072c4186
Gabriel <gabyrivm06@gmail.com>
2025-08-03 19:07:48 -0400

    Logout

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -52,3 +52,18 @@ def login() -> str:
     response.set_cookie(cookie_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+def logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - Empty JSON dictionary with status 200 if successful
+      - 404 if session destruction fails
+    """
+    from api.v1.app import auth
+
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({})
