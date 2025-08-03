53106fb9d9e71a5444cf762cefa3a3bcce89d9a6
Gabriel <gabyrivm06@gmail.com>
2025-08-03 19:18:34 -0400

    Logout

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -54,7 +54,8 @@ def login() -> str:
     return response
 
 
-@app_views.route('/auth_session/logout', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
 def logout() -> str:
     """ DELETE /api/v1/auth_session/logout
     Return:
