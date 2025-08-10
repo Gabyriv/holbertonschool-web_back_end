2ca05585066ea0f360b0ed8b1d0cdc1425ff6bb9
Gabriel <gabyrivm06@gmail.com>
2025-08-10 19:50:47 -0400

    Basic Flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
 from flask import Flask, jsonify
 
 
@@ -6,8 +10,14 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def welcome():
+    """
+    Welcome route
+    """
     return jsonify({"message": "Bienvenue"})
 
 
 if __name__ == "__main__":
+    """
+    Main function
+    """
     app.run(host="0.0.0.0", port="5000")
