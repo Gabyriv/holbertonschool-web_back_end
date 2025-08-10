3c39a6c03ed6db97dd11e9f713095faff35085f1
Gabriel <gabyrivm06@gmail.com>
2025-08-10 19:29:21 -0400

    Hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+from bcrypt import hashpw, gensalt
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password
+    """
+    return hashpw(password.encode('utf-8'), gensalt())
