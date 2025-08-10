936dcee873d3ad0a10c24d2be809ff706aee3661
Gabriel <gabyrivm06@gmail.com>
2025-08-10 19:41:47 -0400

    Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -8,6 +8,13 @@ from bcrypt import hashpw, gensalt
 from sqlalchemy.orm.exc import NoResultFound
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password
+    """
+    return hashpw(password.encode('utf-8'), gensalt())
+
+
 class Auth:
     """
     Auth class to interact with the authentication database.
@@ -23,12 +30,6 @@ class Auth:
             user = self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_password = self._hash_password(password)
+            hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
             return user
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hash a password
-        """
-        return hashpw(password.encode('utf-8'), gensalt())
