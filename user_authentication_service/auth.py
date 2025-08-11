d40b1d6fda323f7cabd579fc3ef28ac65aee9fd9
Gabriel <gabyrivm06@gmail.com>
2025-08-10 20:21:33 -0400

    Generate UUIDs

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from db import DB
 from user import User
 from bcrypt import hashpw, gensalt, checkpw
 from sqlalchemy.orm.exc import NoResultFound
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,6 +14,13 @@ def _hash_password(password: str) -> bytes:
     Hash a password
     """
     return hashpw(password.encode('utf-8'), gensalt())
+
+
+def _generate_uuid() -> str:
+    """
+    Generate a UUID
+    """
+    return str(uuid4())
 
 
 class Auth:
