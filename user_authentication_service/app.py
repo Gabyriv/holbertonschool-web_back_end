4b67cf173939037e25b4cd6a96edc5617d5923a2
Gabriel <gabyrivm06@gmail.com>
2025-08-10 20:04:04 -0400

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,10 @@
 """
 Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
@@ -14,6 +17,20 @@ def welcome():
     Welcome route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user():
+    """
+    Register a new user
+    """
+    try:
+        email = request.form["email"]
+        password = request.form["password"]
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
