4410217ae3eef73ed8e3f754f950e52a388bb94c
Gabriel <gabyrivm06@gmail.com>
2025-08-13 15:45:04 -0400

    Mock HTTP calls

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -8,8 +8,9 @@ expected values for valid nested paths.
 from typing import Any, Mapping, Sequence
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, Mock
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,3 +44,21 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
         self.assertEqual(cm.exception.args[0], missing_key)
+
+
+class TestGetJson(unittest.TestCase):
+    """Tests for `utils.get_json` that avoid real HTTP calls."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url: str, test_payload: dict) -> None:
+        """It returns payload from the mocked requests.get().json()."""
+        with patch("utils.requests.get") as mock_get:
+            mock_resp: Mock = Mock()
+            mock_resp.json.return_value = test_payload
+            mock_get.return_value = mock_resp
+
+            self.assertEqual(get_json(test_url), test_payload)
+            mock_get.assert_called_once_with(test_url)
