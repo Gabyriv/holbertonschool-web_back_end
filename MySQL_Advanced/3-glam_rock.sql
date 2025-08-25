e3af64843329a7b68b26f88018440d8b23a8bfeb
Gabriel <gabyrivm06@gmail.com>
2025-08-24 20:56:12 -0400

    Old school band

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -4,6 +4,7 @@
 -- Use attributes formed and split to compute lifespan. Consider ongoing bands (split NULL or 0) as current year.
 -- Query filters style for 'Glam rock', computes lifespan, orders by lifespan desc
 SELECT band_name,
-       (COALESCE(split, 2025) - formed) AS lifespan
+       (COALESCE(NULLIF(split, 0), YEAR(CURRENT_DATE)) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%';
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
