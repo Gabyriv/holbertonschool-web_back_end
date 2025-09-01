d400971e031ec32a42f8f4a22593a2ffc94a7d8d
Gabriel <gabyrivm06@gmail.com>
2025-08-31 20:02:13 -0400

    Basic Integration testing

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -9,10 +9,14 @@
 const express = require("express");
 const app = express();
 
-app.listen(7865, () => {
-  console.log("API available on localhost port 7865");
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome to the payment system");
 });
+
+if (require.main === module) {
+  app.listen(7865, () => {
+    console.log("API available on localhost port 7865");
+  });
+}
+
+module.exports = app;
